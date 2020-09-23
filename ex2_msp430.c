cc1542b244fa4fa8026b6ce5d20f2d15e35470b2
Felipe <felipeh@alunos.utfpr.edu.br>
2020-09-23 17:06:58 -0300

    Update ex2_msp430.c

--- ex2_msp430.c
+++ ex2_msp430.c
@@ -1,6 +1,58 @@
 #include "msp430gxxxx.h"
- 
-unsigned int valor =0x00;
+
+#define True 1
+#define False 0
+
+unsigned int valor = 0x00;
+char buffersaida = 0xFF;
+char bufferentrada = 0xFF;
+char TXByteCtr = 1; // contagem dos dados
+int Rx = 0
+char LeituraDoComando = 0x3F;
+char UltimoValorLido = 0x5A;
+char ADCLigado = False
+
+//TODO: colocar na norma ANSI
+
+void ConfigurarI2C(unsigned int adresss)
+{
+  _DINT();
+  IE2 |= UCB0RXI                            //Habilita interrupcao RX
+  IE2 |= UCB0TXIE;                          //Habilita interrupcao TX
+  while (UCB0CTL1 & UCTXSTP)
+  {
+	  NULL; //aguarde até o os registradores ficarem livres para fazer o SET
+  }	   
+  UCB0CTL1 |= UCSWRST;                      // SW reset
+  UCB0CTL0 = 0 + UCMODE_3 + UCSYNC;         // configura o I2C: somar com o UCMST para master 
+											// e 0 para slave
+  UCB0CTL1 = UCSSEL_2 + UCSWRST;            // usar o clock SMCLK
+  UCB0BR0 = 12;                             // divisor de clock: 
+  UCB0BR1 = 0;								// fSCL = SMCLK/12
+  UCB0I2CSA = adresss;                      // Endereco do Slave, exemplo: 0x61
+  UCB0CTL1 &= ~UCSWRST;                     // retira o SW reset
+}
+
+void EnviarAssincrono(char saida)
+{
+	//global buffersaida
+	buffersaida = saida
+    while (UCB0CTL1 & UCTXSTP) // aguarde para liberar
+	{}     				       //  (o barramento que passa pelo UCTXSTP precisa ficar solto
+    UCB0CTL1 |= UCTR + UCTXSTT;             // I2C TX: inicia
+    __bis_SR_register(CPUOFF + GIE);        // LPM0 com interrupcao
+}
+
+void ReceberAssincrono()
+{
+	UCB0CTL1 &= ~UCTR ;                  	// Limpa o UCTR
+	while (UCB0CTL1 & UCTXSTP);             // aguarda para liberar 
+	UCB0CTL1 |= UCTXSTT;                    // I2C RX: inicia
+	while (UCB0CTL1 & UCTXSTT);             // aguarda oara liberar o bus
+	UCB0CTL1 |= UCTXSTP;                    // I2C STOP
+	__bis_SR_register(CPUOFF + GIE);        // LPM0 com interrupcao
+	
+}
 
 void ConfigurarADC(void)
 {
@@ -16,7 +68,7 @@ void ConfigurarADC(void)
 
 void main(void)
 {
-   //configuracoes iniciais dos clocks do MSP430 e do whatdog
+   //configuracoes iniciais dos clocks do MSP430 e do Watchdog
    //isto aqui é um porre mesmo, 
    WDTCTL = WDTPW + WDTHOLD; 
    BCSCTL1 = CALBC1_1MHZ;  
@@ -28,29 +80,64 @@ void main(void)
    ConfigurarADC();
 
    __enable_interrupt();  
-
-   while(1)
+   
+   ReceberAssincrono();
+   
+   while(True)
    {
-      //tempo de warmaup do ADC somando o tempo do conversao
-      __delay_cycles(1000);  
+	    while(ADCLigado)
+		{
+			  //tempo de warmaup do ADC somando o tempo do conversao
+			  __delay_cycles(1000);  
 
-      //inicia a conversao com um set start no registrador
-      ADC10CTL0 |= ENC + ADC10SC;
+			  //inicia a conversao com um set start no registrador
+			  ADC10CTL0 |= ENC + ADC10SC;
 
-	  //Habita a interrupcao do LMP0
-      __bis_SR_register(CPUOFF + GIE); 
+			  //Habita a interrupcao para o modo Low Power
+			  __bis_SR_register(CPUOFF + GIE); 
 
-      valor = ADC10MEM; //finalmente faz a leitura
+			  valor = ADC10MEM; //finalmente faz a leitura
+		}
+		//NOPE
    }
 }
 
-//Diretiva para ajustar o vetor de interrupcao
+//Diretivas para ajustar os vetores de interrupcao :
 
+//VETOR DE INTERRUPCAO: modulo para recuperar apos o sleep mode
 #pragma vector=ADC10_VECTOR
-
-//modulo para recuperar apos o sleep mode
 __interrupt void ADC10_ISR (void)
 {
    __bic_SR_register_on_exit(CPUOFF);
+}
 
+//VETOR DE INTERRUPCAO: USCI_B0 Data ISR
+#pragma vector = USCIAB0TX_VECTOR
+__interrupt void USCIAB0TX_ISR(void)
+{
+	if(Rx == True) //Recebendo dados?
+	{                              
+	  bufferentrada = UCB0RXBUF;                   //Pega o Char recebido já disponível no register
+	  __bic_SR_register_on_exit(CPUOFF);           //LPM0
+	  
+	  if(bufferentrada == LeituraDoComando) ADCLigado = True;  
+	  
+	  if(bufferentrada == UltimoValorLido) EnviarAssincrono(valor);
+	  
+	}
+
+	else //Transmitindo dados:
+	{                                       		   
+		  if (TXByteCtr)      //Tamanho máximo do payload               		 
+		  {
+			  UCB0TXBUF = buffersaida;   //envia a char atual             
+			  TXByteCtr--;               //decrementa a posicao atual do buffer até zero           
+		  }
+		  else
+		  {
+			  UCB0CTL1 |= UCTXSTP;                    // I2C STOP
+			  IFG2 &= ~UCB0TXIFG;                     // Limpa flags
+			  __bic_SR_register_on_exit(CPUOFF);      // Sai do moto LP
+		  }
+	}
 }
